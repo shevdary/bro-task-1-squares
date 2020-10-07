e28aab619b043cf857f87707739859949705d4f1
Daryna <shevvdary@gmail.com>
2020-10-07 04:43:55 +0300

    added function for delete button when in the table has just one row&col

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ rmRow.addEventListener("click", deleteRow);
 function deleteRow() {
   let getBody = document.querySelectorAll("tbody")[0].children;
   let childBody = getBody[positionRow];
-  childBody.parentNode.removeChild(childBody);
+  childBody==undefined?getBody.remove(childBody):childBody.parentNode.removeChild(childBody);
 }
 
 block.addEventListener("mousemove", removeCol);
@@ -81,21 +81,27 @@ function deleteCol() {
   let getBody = document.querySelectorAll("tbody")[0].children;
   for (let a of getBody) {
     let childRow = a.children[positionCol];
-    childRow.parentNode.removeChild(childRow);
+    childRow==undefined?getBody.remove(childRow):childRow.parentNode.removeChild(childRow);
   }
 }
 
-square.addEventListener("mouseover", displayPosition);
-function displayPosition(event){
-    rmCol.style.display='block';
-    rmRow.style.display='block';
-    square.style.marginLeft='0px';
+square.addEventListener("mouseover", colCount);
+function colCount(event){
+    rmCol.style.visibility='visible';
+    rmRow.style.visibility='visible';
+    oneLine();
 }
 
-square.addEventListener('mouseout',RowCount)
-function RowCount(event) {
-    rmRow.style.display='none';
-    rmCol.style.display='none';
-    square.style.marginLeft='72px';
+square.addEventListener('mouseout',rowCount)
+function rowCount(event) {
+    rmRow.style.visibility='hidden';
+    rmCol.style.visibility='hidden';
+}
 
+function oneLine() {
+    let getBody = document.querySelectorAll("tbody")[0].children;
+    let countCol=getBody[0].children.length;
+    countCol<2?rmCol.style.visibility='hidden':rmCol.style.visibility='visible';
+    let countRow=getBody.length;
+    countRow<2?rmRow.style.visibility='hidden':rmRow.style.visibility='visible'
 }
