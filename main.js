2ec152e8e5537c6409bc75dd6dfd70a9275d4364
Daryna <shevvdary@gmail.com>
2020-10-09 02:25:37 +0300

    feature: created class for realisation squares

--- main.js
+++ main.js
@@ -1,107 +1,89 @@
-let block = document.querySelector("#rect-block");
-let addColl = document.querySelector(".addCol");
-let addRow = document.querySelector(".addRow");
-let rmRow = document.querySelector(".deleteRow");
-let rmCol = document.querySelector(".deleteCol");
-let square= document.getElementById('square');
-let tbody = document.querySelectorAll("tbody");
-
-let mouseY = 0;
-let positionRow = 0;
-let mouseX = 0;
-let positionCol = 0;
-
-const pixel = "px";
-
-for (let i = 0; i < 4; i++) {
-  createElement("tr", block, "row", i);
-  for (let j = 0; j < 4; j++) {
-    let child = block.lastElementChild;
-    createElement("td", child, "col", j);
+class SuperTable {
+  constructor(selector, rowCount, colCount) {
+    this.$el = document.querySelector(selector);
+    this.col = colCount;
+    this.row = rowCount;
+    this.positionRow = 0;
+    this.positionCol = 0;
+    this.mouseX = 0;
+    this.mouseY = 0;
   }
-}
-
-function createElement(tag, tagParent, className, index) {
-  let tr = document.createElement(tag);
-  tr.setAttribute("class", className);
-  tr.setAttribute("id", index);
-  tagParent.appendChild(tr);
-}
-
-addRow.addEventListener("click", addButtonRow);
-function addButtonRow() {
-  let addRow = document.querySelectorAll("tr");
-  addRow.forEach(item => {
-    createElement("td", item, "col");
-  });
-}
-
-addColl.addEventListener("click", addButtonCol);
-function addButtonCol() {
-  let bodyChildren = tbody[0].children;
-  let childrenLen = bodyChildren[0].children.length;
-  createElement("tr", block, "row");
-  for (let q = 0; q < childrenLen; q++) {
-    let bodyRow = tbody[0].lastElementChild;
-    createElement("td", bodyRow, "col", q);
-  }
-}
-
-block.addEventListener("mousemove", removeRow);
-function removeRow(event) {
-  let e = event;
-  positionRow = e.target.parentElement.id;
-  mouseY = e.target.offsetTop;
-  const move = () => {
-    rmRow.style.marginTop = mouseY+1  + pixel;
+  drawTable = () => {
+    for (let i = 0; i < this.row; i++) {
+      this.createElements("tr", this.$el, "row", i);
+      for (let j = 0; j < this.col; j++) {
+        let child = this.$el.lastElementChild;
+        this.createElements("td", child, "col", j);
+      }
+    }
   };
-  move();
-}
-
-rmRow.addEventListener("click", deleteRow);
-function deleteRow() {
-  let getBody = document.querySelectorAll("tbody")[0].children;
-  let childBody = getBody[positionRow];
-  childBody==undefined?getBody.remove(childBody):childBody.parentNode.removeChild(childBody);
-}
-
-block.addEventListener("mousemove", removeCol);
-function removeCol(event) {
-  let e = event;
-  positionCol = e.target.id;
-  mouseX = e.target.offsetLeft;
-  const move = () => {
-    rmCol.style.left = mouseX+74+ pixel;
+  createElements = (tag, tagParent, className, index) => {
+    const tr = document.createElement(tag);
+    tr.setAttribute("class", className);
+    tr.setAttribute("id", index);
+    tagParent.appendChild(tr);
   };
-  move();
-}
 
-rmCol.addEventListener("click", deleteCol);
-function deleteCol() {
-  let getBody = document.querySelectorAll("tbody")[0].children;
-  for (let a of getBody) {
-    let childRow = a.children[positionCol];
-    childRow==undefined?getBody.remove(childRow):childRow.parentNode.removeChild(childRow);
-  }
-}
+  btnAddCol = () => {
+    const addCol = document.querySelectorAll("tr");
+    for (const item of addCol) {
+      this.createElements("td", item, "col");
+    }
+  };
+  btnAddRow = () => {
+    const tbody = document.querySelectorAll("tbody");
+    const tbodyChildren = tbody[0].children;
+    const childrenLen = tbodyChildren[0].children.length;
+    let index = tbodyChildren.length;
+    this.createElements("tr", this.$el, "row", index);
+    for (let q = 0; q < childrenLen; q++) {
+      let bodyRow = tbody[0].lastElementChild;
+      this.createElements("td", bodyRow, "col", q);
+    }
+  };
+  deleteRow = () => {
+    const getBody = document.querySelectorAll("tbody")[0].children;
+    let childBody = getBody[this.positionRow];
+    childBody == undefined
+      ? getBody.remove(childBody)
+      : childBody.parentNode.removeChild(childBody);
+  };
+  deleteCol = () => {
+    const getBody = document.querySelectorAll("tbody")[0].children;
+    for (let a of getBody) {
+      let childRow = a.children[this.positionCol];
+      childRow.parentNode.removeChild(childRow);
+    }
+  };
+  removeCol = (event, rmCol) => {
+    let e = event;
+    this.positionCol = e.target.id;
+    this.mouseX = e.target.offsetLeft;
+    const move = () => {
+      rmCol.style.left = this.mouseX + 124 + "px";
+    };
+    move();
+  };
+  removeRow = (event, rmRow) => {
+    let e = event;
+    this.positionRow = e.target.parentElement.id;
+    this.mouseY = e.target.offsetTop;
+    const move = () => {
+      rmRow.style.marginTop = this.mouseY + 2 + "px";
+    };
+    move();
+  };
+  colCount = event => {
+    let row = document.querySelector(".deleteRow");
+    let col = document.querySelector(".deleteCol");
+    col.style.display = "inline-block";
+    row.style.display = "inline-block";
+  };
 
-square.addEventListener("mouseover", colCount);
-function colCount(event){
-    rmCol.style.visibility='visible';
-    rmRow.style.visibility='visible';
-    oneLine();
-}
-
-square.addEventListener('mouseout',rowCount)
-function rowCount(event) {
-    rmRow.style.visibility='hidden';
-    rmCol.style.visibility='hidden';
-}
-
-function oneLine() {
-    let getBody = document.querySelectorAll("tbody")[0].children;
-    let countCol=getBody[0].children.length;
-    countCol<2?rmCol.style.visibility='hidden':rmCol.style.visibility='visible';
-    let countRow=getBody.length;
-    countRow<2?rmRow.style.visibility='hidden':rmRow.style.visibility='visible'
+  rowCount = event => {
+    let row = document.querySelector(".deleteRow");
+    let col = document.querySelector(".deleteCol");
+    col.style.display = "inline-block";
+    row.style.display = "inline-block";
+  };
 }
---
+++ superTable.js
@@ -0,0 +1,18 @@
+const Square = new SuperTable("#rect-block", "4", "4");
+Square.drawTable();
+
+const block = document.querySelector("#rect-block");
+const btnCol = document.querySelector(".addCol");
+const btnRow = document.querySelector(".addRow");
+const rmRow = document.querySelector(".deleteRow");
+const rmCol = document.querySelector(".deleteCol");
+const square = document.getElementById("square");
+
+btnCol.addEventListener("click", () => Square.btnAddCol());
+btnRow.addEventListener("click", () => Square.btnAddRow());
+rmRow.addEventListener("click", () => Square.deleteRow());
+rmCol.addEventListener("click", () => Square.deleteCol());
+block.addEventListener("mousemove", e => Square.removeCol(e, rmCol));
+block.addEventListener("mousemove", e => Square.removeRow(e, rmRow));
+square.addEventListener("mouseover", e => Square.colCount(e, rmCol, rmRow));
+square.addEventListener("mouseout", e => Square.rowCount(e, rmCol, rmRow));
